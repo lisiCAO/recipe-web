41649060e0ef57eb9b52c8558f0900bb251f54e4
lisiCAO <caolisi@hotmail.com>
2023-11-16 00:50:27 -0500

    add logo

--- recipe-app/src/components/common/Navbar.jsx
+++ recipe-app/src/components/common/Navbar.jsx
@@ -6,7 +6,9 @@ import './Navbar.scss'; // Your custom styling for the navbar
 const Navbar = ({ onLoginClick, isLoggedIn, userEmail, onLogout }) => {
     return (
         <nav className="navbar">
-            <div className="logo">Logo</div>
+            <div className="logo" >
+                <img src="logo.png" alt="logo" />
+            </div>
             <div className="title">Recipe Admin Panel</div>
             {!isLoggedIn ?
                 <Button onClick={onLoginClick}>Login</Button> :
