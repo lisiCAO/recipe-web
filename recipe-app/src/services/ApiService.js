4c90ce229ed6dd55fb0b69a41b551b55038e827a
lisiCAO <caolisi@hotmail.com>
2023-11-24 09:09:55 -0500

    delete repeated code

--- recipe-app/src/services/ApiService.js
+++ recipe-app/src/services/ApiService.js
@@ -3,7 +3,7 @@
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 const ApiService = {
-  
+
   /* Users */
   // 获取用户列表
   async fetchUsers() {
@@ -68,8 +68,8 @@ const ApiService = {
       return handleError(error);
     }
   },
-   // 获取食谱列表
-   async fetchRecipes() {
+  // 获取食谱列表
+  async fetchRecipes() {
     try {
       const response = await fetch(`${API_BASE_URL}/recipes`);
       return handleResponse(response);
@@ -102,7 +102,7 @@ const ApiService = {
       return handleError(error);
     }
   },
-   
+
   // 更新食谱信息
   async updateRecipe(recipeId, recipeData) {
 
@@ -131,201 +131,172 @@ const ApiService = {
   },
 
   /* Ingredients */
-     // 获取食材列表
-     async fetchIngredients() {
-      try {
-        const response = await fetch(`${API_BASE_URL}/ingredients`);
-        return handleResponse(response);
-      } catch (error) {
-        return handleError(error);
-      }
-    },
-  
-    // 获取特定食材的详情
-    async fetchIngredient(ingredientId) {
-      try {
-        const response = await fetch(`${API_BASE_URL}/ingredients/${ingredientId}`);
-        return handleResponse(response);
-      } catch (error) {
-        return handleError(error);
-      }
-    },
-  
-    // 创建新食材
-    async createIngredient(recipeData) {
-      try {
-        const response = await fetch(`${API_BASE_URL}/ingredients`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: recipeData
-        });
-        return handleResponse(response);
-      } catch (error) {
-        return handleError(error);
-      }
-    },
-     
-    // 更新食材信息
-    async updateIngredient(ingredientId, ingredientData) {
-  
-      try {
-        const response = await fetch(`${API_BASE_URL}/ingredients/${ingredientId}`, {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: ingredientData
-        });
-        return handleResponse(response);
-      } catch (error) {
-        return handleError(error);
-      }
-    },
-  
-    // 删除食材
-    async deleteIngredient(ingredientId) {
-      try {
-        const response = await fetch(`${API_BASE_URL}/ingredients/${ingredientId}`, {
-          method: 'DELETE'
-        });
-        return handleResponse(response);
-      } catch (error) {
-        return handleError(error);
-      }
-    },
+  // 获取食材列表
+  async fetchIngredients() {
+    try {
+      const response = await fetch(`${API_BASE_URL}/ingredients`);
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
 
+  // 获取特定食材的详情
+  async fetchIngredient(ingredientId) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/ingredients/${ingredientId}`);
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  // 创建新食材
+  async createIngredient(recipeData) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/ingredients`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: recipeData
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  // 更新食材信息
+  async updateIngredient(ingredientId, ingredientData) {
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/ingredients/${ingredientId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: ingredientData
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  // 删除食材
+  async deleteIngredient(ingredientId) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/ingredients/${ingredientId}`, {
+        method: 'DELETE'
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  /* Reviews */
+  // 获取评论列表
+  async fetchReviews() {
+    try {
+      const response = await fetch(`${API_BASE_URL}/reviews`);
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  // 获取特定评论的详情
+  async fetchReview(reviewId) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/reviews/${reviewId}`);
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  // 创建新评论
+  async createReview(reviewData) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/reviews`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: reviewData
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  // 更新评论信息
+  async updateReview(reviewId, reviewData) {
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/reviews/${reviewId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: reviewData
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  // 删除评论
+  async deleteReview(reviewId) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/reviews/${reviewId}`, {
+        method: 'DELETE'
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
+
+  /* Uploads */
   // 上传文件
   async uploadFile(formData) {
     try {
-        const response = await fetch(`${API_BASE_URL}/upload`, {
-            method: 'POST',
-            body: formData
-        });
-        return handleResponse(response);
-    } catch (error) {
-        return handleError(error);
-    }
-},
-// 登录用户
-async login(credentials) {
-  try {
-    const response = await fetch(`${API_BASE_URL}/login`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(credentials)
-    });
-    const data = await handleResponse(response);
-    // 保存JWT到localStorage
-    localStorage.setItem('token', data.token);
-    return data;
-  } catch (error) {
-    return handleError(error);
-  }
-},
-
-/* Reviews */
-// 获取评论列表
-async fetchReviews() {
-  try {
-    const response = await fetch(`${API_BASE_URL}/reviews`);
-    return handleResponse(response);
-  } catch (error) {
-    return handleError(error);
-  }
-},
-
-// 获取特定评论的详情
-async fetchReview(reviewId) {
-  try {
-    const response = await fetch(`${API_BASE_URL}/reviews/${reviewId}`);
-    return handleResponse(response);
-  } catch (error) {
-    return handleError(error);
-  }
-},
-
-// 创建新评论
-async createReview(reviewData) {
-  try {
-    const response = await fetch(`${API_BASE_URL}/reviews`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: reviewData
-    });
-    return handleResponse(response);
-  } catch (error) {
-    return handleError(error);
-  }
-},
-
-// 更新评论信息
-async updateReview(reviewId, reviewData) {
-
-  try {
-    const response = await fetch(`${API_BASE_URL}/reviews/${reviewId}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: reviewData
-    });
-    return handleResponse(response);
-  } catch (error) {
-    return handleError(error);
-  }
-},
-
-// 删除评论
-async deleteReview(reviewId) {
-  try {
-    const response = await fetch(`${API_BASE_URL}/reviews/${reviewId}`, {
-      method: 'DELETE'
-    });
-    return handleResponse(response);
-  } catch (error) {
-    return handleError(error);
-  }
-},
-
-/* Uploads */
-// 上传文件
-async uploadFile(formData) {
-try {
-    const response = await fetch(`${API_BASE_URL}/upload`, {
+      const response = await fetch(`${API_BASE_URL}/upload`, {
         method: 'POST',
         body: formData
-    });
-    return handleResponse(response);
-} catch (error) {
-    return handleError(error);
-}
-},
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return handleError(error);
+    }
+  },
 
-/* Auth */
-// 登录用户
-async login(credentials) {
-try {
-const response = await fetch(`${API_BASE_URL}/login`, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify(credentials)
-});
-const data = await handleResponse(response);
-// 保存JWT到localStorage
-localStorage.setItem('token', data.token);
-return data;
-} catch (error) {
-return handleError(error);
-}
-},
+  /* Auth */
+  // 登录用户
+  async login(credentials) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(credentials)
+      });
+      const data = await handleResponse(response);
+      // 保存JWT到localStorage
+      localStorage.setItem('token', data.token);
+      return data;
+    } catch (error) {
+      return handleError(error);
+    }
+  },
 
-// 获取当前用户信息
-// async fetchCurrentUser() {
-//   try {
-//     const response = await fetch(`${API_BASE_URL}/me`, {
-//       headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
-//     });
-//     return handleResponse(response);
-//   } catch (error) {
-//     return handleError(error);
-//   }
-// }
+  // 获取当前用户信息
+  // async fetchCurrentUser() {
+  //   try {
+  //     const response = await fetch(`${API_BASE_URL}/me`, {
+  //       headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+  //     });
+  //     return handleResponse(response);
+  //   } catch (error) {
+  //     return handleError(error);
+  //   }
+  // }
 
   // 为其他 API 路由添加类似的方法（如 recipes, ingredients, reviews）
 
@@ -333,19 +304,19 @@ return handleError(error);
 
 /* 处理 API 响应 */
 const handleResponse = async (response) => {
-    if (response.ok) {
-      const contentType = response.headers.get('Content-Type');
-      if (contentType && contentType.includes('application/json')) {
-          return await response.json();
-      } else {
-          // 处理非 JSON 响应
-          const text = await response.text();
-          throw new Error(`Non-JSON response: ${text}`);
-      }
+  if (response.ok) {
+    const contentType = response.headers.get('Content-Type');
+    if (contentType && contentType.includes('application/json')) {
+      return await response.json();
+    } else {
+      // 处理非 JSON 响应
+      const text = await response.text();
+      throw new Error(`Non-JSON response: ${text}`);
+    }
   } else {
-      // 处理 HTTP 错误状态
-      const errorText = await response.text();
-      throw new Error(`HTTP error ${response.status}: ${errorText}`);
+    // 处理 HTTP 错误状态
+    const errorText = await response.text();
+    throw new Error(`HTTP error ${response.status}: ${errorText}`);
   }
 }
 
